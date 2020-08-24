f4647b386036fd63c133e5cf38c5615cd13c3cca
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2020-08-24 17:41:20 +0200

    collatz-conjecture submit

---
+++ collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,9 @@
+export const steps = n => {
+  if (n <= 0) throw new Error('Only positive numbers are allowed');
+  let count = 0;
+  while(n !== 1) {
+    n = n % 2 == 0 ? n / 2 : 3 * n + 1
+    count++;
+  }
+  return count;
+};
