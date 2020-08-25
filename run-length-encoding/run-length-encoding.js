a8132c664c25882db9ecb9b053ca583c7ffe3da2
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2020-08-25 23:08:49 +0200

    run-length-encoding regex submit

--- run-length-encoding/run-length-encoding.js
+++ run-length-encoding/run-length-encoding.js
@@ -1,24 +1,3 @@
-export const encode = string => {
-  if (!string) return '';
-  const chars = [...string];
-  let charCountPairs = [{char : chars[0], count: 0}];
-  for (const ch of chars) {
-    const lastCharCountPair = charCountPairs[charCountPairs.length - 1];
-    if (lastCharCountPair['char'] === ch) lastCharCountPair['count']++;
-    else charCountPairs.push({char : ch, count : 1});
-  }
-  return charCountPairs.reduce((res, pair) => res + (pair['count'] > 1 ? pair['count'] : '') + pair['char'], '');
-};
+export const encode = (str) => str.replace(/(.)\1+/g, (match, char) => match.length + char);
 
-export const decode = string => {
-  if (!string) return '';
-  return [...string.matchAll(/(\d*[\s|\w])/g)]
-  .map(match => match[0])
-  .map(group => {
-    if (group.length === 1) return group;
-    const count = parseInt(group.match(/\d+/)[0]);
-    const char = group.match(/\D/)[0];
-    return char.repeat(count);
-  })
-  .join('');
-};
+export const decode = (str) => str.replace(/(\d+)(.)/g, (_match, count, char) => char.repeat(count));
